62d908fb58f461f11b6d268fdbd56d177f03298e
98Nishi <nishamaurya7499@gmail.com>
2023-08-12 19:05:19 +0530

    game completed

--- Bubble-Game/script.js
+++ Bubble-Game/script.js
@@ -2,27 +2,55 @@ document.addEventListener("DOMContentLoaded", function () {
   const startButton = document.querySelector("#startButton");
   const topPanel = document.querySelector("#top");
   const bottomPanel = document.querySelector("#bottom");
+  const bgMusic = document.querySelector("#bgMusic");
 
   startButton.addEventListener("click", function () {
+    bgMusic.play();
+
     startButton.style.display = "none";
     topPanel.style.display = "flex";
     bottomPanel.style.display = "flex";
+    let isMusicPlaying = false;
+
+    // Function to play/pause the music
+    function toggleMusic() {
+      if (isMusicPlaying) {
+        bgMusic.pause();
+      } else {
+        bgMusic.play();
+      }
+      isMusicPlaying = !isMusicPlaying;
+      updateMusicControlText();
+    }
+
+    function updateMusicControlText() {
+      musicControl.textContent = isMusicPlaying ? "Pause" : "Play";
+    }
+
+    musicControl.addEventListener("click", toggleMusic);
+
+    startButton.addEventListener("click", function () {
+      bgMusic.play();
+    });
 
     var timer = 60;
     var score = 0;
     var hitNum = 0;
 
+    // function for increasing score
     function increaseScore() {
       score += 10;
 
       document.querySelector("#scoreVal").textContent = score;
     }
 
+    // function for newhit
     function newHit() {
       hitNum = Math.floor(Math.random() * 10);
       document.querySelector("#hitVal").textContent = hitNum;
     }
 
+    // Function to make bubbles
     function makeBubble() {
       let mess = "";
 
@@ -34,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector("#bottom").innerHTML = mess;
     }
 
+    // Function to run timer
     function runTimer() {
       var time = setInterval(function () {
         if (timer > 0) {
@@ -48,7 +77,10 @@ document.addEventListener("DOMContentLoaded", function () {
       }, 1000);
     }
 
-    document.querySelector("#bottom").addEventListener("click", function (details) {
+    // Checking clicked number and hit number is same or not
+    document
+      .querySelector("#bottom")
+      .addEventListener("click", function (details) {
         var clickedNum = Number(details.target.textContent);
 
         if (clickedNum === hitNum) {
